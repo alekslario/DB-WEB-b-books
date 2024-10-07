abb86b28ea7c8f28e7956a8cedb36aeac6c63192
Alex <admin@aleksandrslarionovs.com>
2024-10-07 13:25:49 +0100

    Add init script

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#mysql
+sudo apt update
+sudo apt install mysql-server
+sudo systemctl start mysql.service
+# create random password
+PASSWDDB="123"
+MAINDB="books"
+# replace "-" with "_" for database username
+USER="alari001"
+#create database
+
+sudo mysql <<MYSQL_SCRIPT
+CREATE DATABASE IF NOT EXISTS  ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';
+GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USER}'@'localhost';
+FLUSH PRIVILEGES;
+USE ${MAINDB};
+source create_db.sql;
+MYSQL_SCRIPT
