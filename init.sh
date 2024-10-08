c7d9b1139e5218cee3c3ac22432237af52b9c565
Alex <admin@aleksandrslarionovs.com>
2024-10-08 12:01:28 +0100

    Fix

--- init.sh
+++ init.sh
@@ -4,15 +4,15 @@ sudo apt update
 sudo apt install mysql-server
 sudo systemctl start mysql.service
 # create random password
-PASSWDDB="123"
+PASSWDDB="1234"
 MAINDB="bettys_books"
 # replace "-" with "_" for database username
-USER="'alari001"
+USER="alari001"
 #create database
 
 sudo mysql <<MYSQL_SCRIPT
 CREATE DATABASE IF NOT EXISTS  ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;
-CREATE USER IF NOT EXISTS '${USER}'@'localhost' IDENTIFIED BY '${PASSWDDB}';
+CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';
 GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USER}'@'localhost';
 FLUSH PRIVILEGES;
 USE ${MAINDB};
